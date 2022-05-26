26e981f05d3f28184d004cd7ccad470debc9f5ae
nina07 <niyanta.zamindar@gmail.com>
2022-05-26 21:47:16 +0530

    created model author, author type class, experimented with fecthing records

---
+++ app/graphql/types/author_type.rb
@@ -0,0 +1,21 @@
+class Types::AuthorType < Types::BaseObject
+  description "Author record"
+
+  field :id, ID, null: false
+  field :first_name, String, null: false
+  field :last_name, String, null: true
+  field :yob, Int, null: false
+  field :is_alive, Boolean, null: false
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+  field :full_name, String, null: true
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
+
+  field :coordinates, Types::CoordinatesType, null: false
+
+  field :publication_years, [Int], null: false
+end
