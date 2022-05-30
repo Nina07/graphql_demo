d274a86adfc029b21555a3f7be711186f3b8d64f
nina07 <niyanta.zamindar@gmail.com>
2022-05-30 14:03:03 +0530

    visibility and authorization through Graphql

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,8 @@ class Types::UserType < Types::BaseObject
   field :email, String, null: false
   field :password, String, null: false
   field :is_super_admin, Boolean, null: false
+
+  def self.visible?(context) #hides current_user info from schema if the user is not logged in
+    !!context[:current_user]
+  end
 end
